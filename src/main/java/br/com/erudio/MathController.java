5c1ab98ba198cebd90b4311f3413651783256dcd
Jackson santos <jackson1santos@hotmail.com>
2023-12-11 21:01:25 -0300

    inserindo formulas matematicas pelo endpoint

--- src/main/java/br/com/erudio/MathController.java
+++ src/main/java/br/com/erudio/MathController.java
@@ -1,6 +1,7 @@
 package br.com.erudio;
 
 
+import br.com.erudio.exceptions.UnsupportedMathOperationException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,16 +15,85 @@ public class MathController {
     // cria ID conforme o objeto é criado
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-    public Double sum(
+    // soma
+    @RequestMapping(value = "/soma/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double soma(
             @PathVariable(value = "numberOne") String numberOne,
             @PathVariable(value = "numberTwo") String numberTwo
     ) throws Exception {
 
         if (!isNumeric(numberOne) || (!isNumeric(numberTwo))) {
-            throw new Exception();
+            throw new UnsupportedMathOperationException("Please set a numeric value");
         }
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    //subtracao
+    @RequestMapping(value = "/subtracao/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double subtracao(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) throws Exception {
+
+        if (!isNumeric(numberOne) || (!isNumeric(numberTwo))) {
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        }
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+    }
+
+    //multiplicacao
+    @RequestMapping(value = "/multiplicao/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double multiplicao(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) throws Exception {
+
+        if (!isNumeric(numberOne) || (!isNumeric(numberTwo))) {
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        }
+        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+    }
+
+    //divisao
+    @RequestMapping(value = "/divisao/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double divisao(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) throws Exception {
+
+        if (!isNumeric(numberOne) || (!isNumeric(numberTwo))) {
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        }
+        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+    }
+
+    //media
+    @RequestMapping(value = "/media/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double media(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) throws Exception {
+
+        if (!isNumeric(numberOne) || (!isNumeric(numberTwo))) {
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        }
+        return convertToDouble(numberOne) + convertToDouble(numberTwo) / 2;
+    }
+
+    //raizquadrada
+    @RequestMapping(value = "/raizquadrada/{numberOne}", method = RequestMethod.GET)
+    public Double raizquadrada(
+            @PathVariable(value = "numberOne") String numberOne
+
+    ) throws Exception {
+
+        if (!isNumeric(numberOne)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        }
+
+        Double rquadrada = Math.sqrt(convertToDouble(numberOne));
+
+        return rquadrada;
     }
 
     private Double convertToDouble(String strNumber) {
